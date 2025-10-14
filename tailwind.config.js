78d3aae1de28744ebb3b0cf2435a492824e32336
Henry-Elois <elois.henry@edu.univ-fcomte.fr>
2025-10-14 17:39:43 +0200

    theme daisyui

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{astro,html,js,jsx,ts,tsx}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [require("daisyui")],
+  daisyui: {
+    themes: [
+      "garden", 
+      "night", 
+    ],
+    darkTheme: "night", 
+  },
+};
